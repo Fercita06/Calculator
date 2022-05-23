e0958fc974b916ab77215ab3a536b482f58f2b06
suarezfer1712 <suarezfer1712@gmail.com>
2022-05-23 15:01:29 +0100

    added the stwitched operator

--- main.js
+++ main.js
@@ -1,19 +1,70 @@
-
 const numberDisplay = document.getElementById("display");
 const numberButtons = document.querySelectorAll(".calculator__button");
-console.log("numberButtons");
+//console.log("numberButtons");
+const operatorButtons = document.querySelectorAll(".button calculator__button--operator");
+const equalButton = document.getElementById("button-equal");
+console.log(equalButton);
 
+const dotButton = document.getElementById("button-dot");
+
+let firstNumber="";
+let secondNumber="";
+let operator= "";
+
+//display the value on the display
 const showInputs = (event) => {
 numberDisplay.innerHTML += event.target.innerText;   
 }
 numberButtons.forEach(button => {
 button.addEventListener("click", showInputs)
+
+
+//if the operator button is clicked , the second number appends to itself.
+if(firstNumber && operator){
+  secondNumber= secondNumber + EventTarget.innerHTML;
+}
+
 });
 
-const OperatorButtons = document.querySelectorAll(".button calculator__button--operator");
+//Used the forEach to add and eventListener to the operators
+operatorButtons.forEach((symbol) =>{
+  symbol.addEventListener("click", (event)=>{
+    console.log(event.target.innerHTML);
+  })
 
+});
 
+const calculate = ()=>{
+  
+  switch(operator){   
+    case"+":
+    return firstNumber + secondNumber;
+    break;
 
+    case"-":
+    return firstNumber - secondNumber;
+    break;
+
+    case"*":
+    return firstNumber * secondNumber;
+    break;
+
+    case"/":
+    return firstNumber / secondNumber;
+    break;
+
+    case"%":
+    return firstNumber + secondNumber;
+    break;
+  }
+    
+
+}
+//console.log(calculate);
+equalButton.addEventListener("click", calculate)
 
 
  
+   
+
+
